9d222394e479dd5c25ae2166c96d340147ddbc9f
savakroth <99940837+savakroth@users.noreply.github.com>
2023-11-23 09:23:42 +0700

    Update recursion.java

--- recursion.java
+++ recursion.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 public class recursion {
   public static void main(String[] args) {
 
-    //call your recursion fuction
-    //print result
+    //initiating Scanner
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a String: ");
+    String given_string = scanner.nextLine();
 
-   }
+    //calling the function and printing output
+    System.out.println("The reversed value of the given string is "+ recursionFunction(given_string));
+   
+  scanner.close();
+  }
     
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+    public static String recursionFunction(String given_String){
+      if (given_String.isEmpty()){
+        return given_String;}
+      //Calling Function Recursively
+      return recursionFunction(given_String.substring(1)) + given_String.charAt(0);
     }
-  
 }
-    
